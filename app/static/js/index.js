c862d0a6b063e95dbe76ad9823e19bb2980cb0b1
Garvit Dewan <d.garvit@gmail.com>
2018-04-12 06:41:46 +0530

    Add feature for ETH exchange, different currency exchanges

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,70 +1,75 @@
 var app = angular.module('coinex', []);
-app.controller('myController', function ($scope, $http) {
+app.controller('myController', function ($scope, $http, $q) {
 	$scope.profit = 0;
-	var start = new Date();
-	var start_string;
-	var end_string;
-	var end = new Date();
-	var price;
-
-	var dateToString = function(date) {
-		var tzoffset = date.getTimezoneOffset() * 60000; //offset in milliseconds
-		var localISOTime = (new Date(date - tzoffset)).toISOString().substring(0, 10);
-		return localISOTime;
-	};
-
-	var setDates = function() {
-		start_string = dateToString($scope.purchase_date);
-		end_string = dateToString($scope.sell_date);
+	$scope.proloss = "profit";
+	$scope.coin = 'BTC';
+	$scope.currency = 'USD';
+	$scope.symbol = '$';
+	$scope.amount = 0;
+	$scope.tax = 0;
+	var toTimestamp = function(strDate) {
+		var datum = Date.parse(strDate);
+		return datum/1000;
 	}
-	
-	var retreiveTable = function(callback) {
-		$http.get('https://api.coindesk.com/v1/bpi/historical/close.json', {
+
+	var getPriceOn = function(date, callback) {
+		$http.get('https://min-api.cryptocompare.com/data/dayAvg', {
 			params: {
-				'start': start_string,
-				'end': end_string,
+				fsym: $scope.coin,
+				tsym: $scope.currency,
+				toTs: toTimestamp(date),
 			}
-		}).then(function(data) {
-			price = data.data.bpi;
-			console.log(price);
-			callback();
+		}).then(function(response) {
+			console.log(response.data[$scope.currency]);
+			callback(response.data[$scope.currency]);
 		});
-	};
+	}
 
 	var calculate_profit = function() {
-		console.log(end_string);
-		console.log(start_string);
-		console.log(price);
-		console.log(price[end_string]);
-		console.log(price[start_string]);
-		return (price[end_string] - price[start_string]) * $scope.amount;
+
+		return (sell_price - purchase_price) * $scope.amount;
 	};
 
-	$scope.calculate = function() {
-		console.log($scope.purchase_date);
-		console.log(dateToString($scope.purchase_date));	
+	$scope.changeSymbol = function() {
+		switch ($scope.currency) {
+			case 'INR':
+				$scope.symbol = '₹';
+				break;
+			case 'USD':
+				$scope.symbol = '$';
+				break;
+			case 'EUR':
+				$scope.symbol = '€';
+				break;
+			case 'JPY':
+				$scope.symbol = '¥';
+				break;
+			default:
+				$scope.symbol = '$';
+		}
+	};
+
+	$scope.calculate = function() {	
 		if ($scope.amount === undefined || $scope.amount < 0) {
-			document.getElementById('error').innerHTML = "Please enter a valid amount.";
 			throw("Amount is invalid or not defined.");
 		}
 
-		if ($scope.purchase_date === undefined || $scope.sell_date === undefined) {	
-			document.getElementById('error').innerHTML = "Please enter a valid date.";
+		if ($scope.purchase_date === undefined || $scope.sell_date === undefined) {
 			throw("Date not defined.");
 		}
 
-		if ((start > $scope.purchase_date) || (end < $scope.sell_date)) {
-			start = $scope.purchase_date;
-			end = $scope.sell_date;
-			setDates();
-			retreiveTable(function() {
-				$scope.profit = calculate_profit();
+		getPriceOn($scope.purchase_date, function(data) {
+			$scope.purchase_price = data;
+
+
+			getPriceOn($scope.sell_date, function(response) {
+				$scope.sell_price = response;
+				$scope.profit = ($scope.sell_price - $scope.purchase_price) * $scope.amount;
+				$scope.tax = ($scope.profit > 0) ? ($scope.profit * 0.3) : 0;
+				$scope.proloss = ($scope.profit > 0) ? "profit" : "loss";
 			});
-		}
-		else {
-			setDates();
-			$scope.profit = calculate_profit();
-		}
-		//$scope.$apply();
+		});
 	};
-});
+}).filter('makePositive', function() {
+    return function(num) { return Math.abs(num); }
+})
